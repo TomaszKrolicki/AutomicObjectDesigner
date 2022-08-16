0694ec1ac0c512625da14c0ade1f5f89adcdb09d
Tomasz Królicki <tomek.krolicki@gmail.com>
2022-08-16 14:19:57 +0200

    Added Initial migration

--- AutomicObjectDesigner/AutomicObjectDesignerBack/Data/AppDatabaseContext.cs
+++ AutomicObjectDesigner/AutomicObjectDesignerBack/Data/AppDatabaseContext.cs
@@ -10,6 +10,7 @@ namespace AutomicObjectDesignerBack.Data
         public AppDatabaseContext(DbContextOptions<AppDatabaseContext> options) : base(options)
         {
         }
+
         DbSet<User> Users { get; set; }
         DbSet<FileTransfer> FileTransfers { get; set; }
         DbSet<LinuxSimple> LinuxSimple { get; set; }
@@ -18,5 +19,65 @@ namespace AutomicObjectDesignerBack.Data
         DbSet<WindowsSimple> WindowsSimple { get; set; }
 
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .ToTable("User");
+            modelBuilder.Entity<User>()
+                .Property(s => s.Id)
+                .IsRequired(true);
+            modelBuilder.Entity<User>()
+                .Property(s => s.FirstName)
+                .IsRequired(true);
+            modelBuilder.Entity<User>()
+                .Property(s => s.LastName)
+                .IsRequired(true);
+            modelBuilder.Entity<User>()
+                .Property(s => s.UserName)
+                .IsRequired(true);
+            modelBuilder.Entity<User>()
+                .Property(s => s.Email)
+                .IsRequired(true);
+            modelBuilder.Entity<User>()
+                .Property(s => s.Password)
+                .IsRequired(true);
+            modelBuilder.Entity<User>()
+                .Property(s => s.HasEmailConfirmed)
+                .IsRequired()
+                .HasDefaultValue(false);
+            modelBuilder.Entity<User>()
+                .Property(s => s.IsAdministrator)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+            modelBuilder.Entity<User>()
+                .HasData(
+                    new User
+                    {
+                        Id = 1,
+                        FirstName = "John",
+                        LastName = "Doe",
+                        UserName = "12345",
+                        Email = "Johndoe@gmail.com",
+                        Password = "admin",
+                        HasEmailConfirmed = true,
+                        IsAdministrator = true
+
+                    },
+                    new User
+                    {
+                        Id = 2,
+                        FirstName = "Eva",
+                        LastName = "Doe",
+                        UserName = "34567",
+                        Email = "evathe2@gmail.com",
+                        Password = "haslo",
+                        HasEmailConfirmed = true,
+                        IsAdministrator = false
+
+                    }
+                );
+        }
     }
 }
