c793e3790aa729b9a517d2c0fe8006324e7645cb
Tomasz Królicki <tomek.krolicki@gmail.com>
2022-08-19 00:20:24 +0200

    SapSimple fixed

--- AutomicObjectDesigner/AutomicObjectDesignerBack/Controllers/ObjectContollers/DataService.cs
+++ AutomicObjectDesigner/AutomicObjectDesignerBack/Controllers/ObjectContollers/DataService.cs
@@ -17,30 +17,43 @@ namespace AutomicObjectDesignerBack.Controllers.ObjectContollers
             {
                 new SapSimple
                 {
-                    Agent = "Tadzio",
+                    SapReport = "QSW.102#ZZZ#PAYMENT#MYREP_01$VAR_101.JOBS",
+                    SapJobName = "NAME1_IN_SAP",
+                    SapVariant = "SAP.JOB",
+                    Agent = "Agent1",
                     Active = true,
                     DeleteSapJob = false,
                     Folder = "Document",
                     Id = 5,
-                    Login = "Tadzio",
+                    Login = "Agent1",
+                    Queue = "QSW102_A.QUEUE",
                     MaxParallelTasks = 5,
                     OwnerId = 4,
-                    PorcessName = "process",
-                    PostProcess = "postprocess",
+                    Process = "Process",
+                    PorcessName = "R3_ACTIVATE_REPORT REPORT=&SAP_REPORT#,VARIANT=&SAP_VARIANT#",
+                    PreProcess = ":INC XXX.XXX#ZZZ#SAP_PRE#GENERAL.JOBI",
+                    PostProcess = ":INC XXX.XXX#ZZZ#SAP_POST#GENERAL.JOBI",
 
                 },
                 new SapSimple
                 {
-                Agent = "Adzio",
-                Active = true,
-                DeleteSapJob = false,
-                Folder = "Document",
-                Id = 6,
-                Login = "Adzio",
-                MaxParallelTasks = 6,
-                OwnerId = 5,
-                PorcessName = "process",
-                PostProcess = "postprocess",
+                    
+                    SapReport = "QSW.102#ZZZ#PAYMENT#MYREP_01$VAR_101.JOBS",
+                    SapJobName = "NAME1_IN_SAP", 
+                    SapVariant = "SAP.JOB",
+                    Agent = "Agent2",
+                    Active = true,
+                    DeleteSapJob = false,
+                    Folder = "Document",
+                    Id = 6,
+                    Login = "Agent2",
+                    Queue = "QSW102_A.QUEUE",
+                    MaxParallelTasks = 6,
+                    OwnerId = 5,
+                    Process = "Process",
+                    PorcessName = "R3_ACTIVATE_REPORT REPORT=&SAP_REPORT#,VARIANT=&SAP_VARIANT#",
+                    PreProcess = ":INC XXX.XXX#ZZZ#SAP_PRE#GENERAL.JOBI",
+                    PostProcess = ":INC XXX.XXX#ZZZ#SAP_POST#GENERAL.JOBI",
 
                 }
             };
