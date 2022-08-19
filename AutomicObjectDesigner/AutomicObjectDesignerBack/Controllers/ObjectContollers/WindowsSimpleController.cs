a9dc64efc35532353d58e1df33e942a25e5157a1
Daniel <d_gorski@interia.eu>
2022-08-19 09:09:12 +0200

    Create Base Class

--- AutomicObjectDesigner/AutomicObjectDesignerBack/Controllers/ObjectContollers/WindowsSimpleController.cs
+++ AutomicObjectDesigner/AutomicObjectDesignerBack/Controllers/ObjectContollers/WindowsSimpleController.cs
@@ -1,14 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutomicObjectDesigner.Models.Objects;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AutomicObjectDesignerBack.Controllers;
 
 [ApiController]
 public class WindowsSimpleController : Controller
 {
+    [Route("/api/WindowsSimple")]
     [HttpPost]
-    [Route("/api/[controller]")]
-    public string WindowsSimple()
+    public IActionResult GetWindowsSimple(WindowsSimple windowsSimple)
     {
-        return "";
+        windowsSimple.id = 1;
+        windowsSimple.ProcessName = "Process";
+        windowsSimple.NameSuffix = "Suffix";
+        windowsSimple.ObjectName = "sid1.client1#ZZZ#Process#WIN_Suffix.JOBS"
+        if (windowsSimple.ObjectName == "sid1.client1#ZZZ#Process#WIN_Suffix.JOBS")
+        {
+            windowsSimple.Process = "";
+            windowsSimple.Docu = "";
+            windowsSimple.Title = "";
+            windowsSimple.Archive1 = "";
+            windowsSimple.Archive2 = "";
+            windowsSimple.InternalAccount = "account";
+            // SaveInDatabase(WindowsSimple)
+        }
+        return Ok(windowsSimple);
+        //Go to next website
     }
 }
--- AutomicObjectDesigner/AutomicObjectDesignerBack/Models/Objects/WindowsSimple.cs
+++ AutomicObjectDesigner/AutomicObjectDesignerBack/Models/Objects/WindowsSimple.cs
@@ -1,6 +1,40 @@
-﻿namespace AutomicObjectDesigner.Models.Objects;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutomicObjectDesigner.Models.Objects;
 
 public class WindowsSimple : Job
 {
-    
+    public int id { get; set; }
+    //Step 1:
+    public enum EnumWinServer { server1, server2 ,server3 }
+    public enum EnumWinLogin { login1, login2, login3 }
+    public enum EnumSapSid { sid1, sid2 , sid3 }
+    public enum EnumSapClient { client1, client2 , client3 }
+    public enum EnumRoutineJob { yes, no }
+    [Required]
+    [StringLength(7)]
+    public string? ProcessName { get; set; }
+    [Required]
+    [MaxLength(40)]
+    public string? NameSuffix { get; set; }
+    //Step 2:
+    public string? ObjectName { get; set; }
+    //Step 3:
+    public string? ProcessInfo { get; set; }
+    //Step 4:
+    public string? Docu { get; set; }
+    //Step 5:
+    [Required]
+    [MaxLength(200)]
+    public string? Title { get; set; }
+    [Required]
+    [MaxLength(60)]
+    public string? Archive1 { get; set; }
+    [Required]
+    [MaxLength(20)]
+    public string? Archive2 { get; set; }
+    [Required]
+    [MaxLength(16)]
+    public string? InternalAccount { get; set; }
+    public string? Folder { get; set; }
 }
