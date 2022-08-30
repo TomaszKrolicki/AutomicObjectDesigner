da432b7ba22f8cc77f1cad1b6f5c33f23fe31722
Leon Stawecki <stawecki.media@gmail.com>
2022-08-30 10:47:32 +0200

    Added pages

---
+++ AutomicObjectDesigner/AutomicObjectDesignerFront/src/pages/AutomicObjectDesigner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Header } from '../components/Header';
+import { useStateContext } from '../contexts/ContextProvider';
+
+export const AutomicObjectDesigner = () => {
+  return (
+    <div className="mt-24">
+    </div>
+  )
+}
