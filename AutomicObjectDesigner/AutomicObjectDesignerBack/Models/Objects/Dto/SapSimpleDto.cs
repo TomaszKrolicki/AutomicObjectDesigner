2d6b3f611b8210d35cb27827b6d2e5a8e5cea661
Daniel <d_gorski@interia.eu>
2022-09-05 09:43:10 +0200

    dto

--- AutomicObjectDesigner/AutomicObjectDesignerBack/Models/Objects/Dto/SapSimpleDto.cs
+++ AutomicObjectDesigner/AutomicObjectDesignerBack/Models/Objects/Dto/SapSimpleDto.cs
@@ -7,9 +7,13 @@ namespace AutomicObjectDesigner.Models.Objects;
 // NEED TO ADD ALL THINGS HERE: NAME ETC.
 public class SapSimpleDTO
 {
+    public string? SapClient { get; set; }
+    public string? SapSid { get; set; }
+    public string? SapReport { get; set; }
 
+    public string? SapVariant { get; set; }
     public bool? RoutineJob { get; set; }
-    public bool? DeleteSapJobBool { get; set; }
-
-
+    public string? ProcessName { get; set; }
+    public string? SapJobName { get; set; }
+    public bool? DeleteSapJob { get; set; }
 }
