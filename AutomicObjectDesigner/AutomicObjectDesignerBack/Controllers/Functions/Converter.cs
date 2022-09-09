ff7fd0b4a62226e6155a94af12afc86d00010d8e
Leon Stawecki <stawecki.media@gmail.com>
2022-09-09 13:41:17 +0200

    TextKetteConverter added

---
+++ AutomicObjectDesigner/AutomicObjectDesignerBack/Controllers/Functions/Converter.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AutomicObjectDesignerBack.Controllers.Functions;
+
+public class Converter
+{
+
+    public string TextKetteConverter(string text)
+    {
+        Regex reg = new Regex("[!\"#$%&'()*+,./:;<=>?@^`{|}~]");
+        text = reg.Replace(text, "-");
+
+        return text;
+    }
+}
