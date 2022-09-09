99c8a14b6aabe0e185475e1bb76b884d39962217
Leon Stawecki <stawecki.media@gmail.com>
2022-09-09 13:54:55 +0200

    Added converter functions

--- AutomicObjectDesigner/AutomicObjectDesignerBack/Controllers/Functions/Converter.cs
+++ AutomicObjectDesigner/AutomicObjectDesignerBack/Controllers/Functions/Converter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace AutomicObjectDesignerBack.Controllers.Functions;
 
@@ -8,6 +9,28 @@ public class Converter
     public string TextKetteConverter(string text)
     {
         Regex reg = new Regex("[!\"#$%&'()*+,./:;<=>?@^`{|}~]");
+        text = reg.Replace(text, "-");
+
+        return text;
+    }
+
+    public string ConvertText(Regex reg, string text, string replacmentSign)
+    {
+        text = reg.Replace(text, replacmentSign);
+
+        return text;
+    }
+
+    public string TextReportConverter(string text)
+    {
+        Regex reg = new Regex("[!\"%&'()*+,./:;<=>?^`{|}~]");
+        text = reg.Replace(text, "-");
+
+        return text;
+    }
+    public string TextVariantConverter(string text)
+    {
+        Regex reg = new Regex("[!\"#$%&'()*+_,./:;<=>?@^`{|}~]");
         text = reg.Replace(text, "-");
 
         return text;
