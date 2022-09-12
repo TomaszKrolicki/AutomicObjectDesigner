0cd6bc1e982f8f43223cf765f91b1e4220d41bca
Daniel <d_gorski@interia.eu>
2022-09-12 11:06:45 +0200

    merge

--- AutomicObjectDesigner/AutomicObjectDesignerBack/Models/Objects/Dto/SapJobBWChainDto.cs
+++ AutomicObjectDesigner/AutomicObjectDesignerBack/Models/Objects/Dto/SapJobBWChainDto.cs
@@ -1,13 +1,29 @@
-﻿namespace AutomicObjectDesignerBack.Models.Objects.Dto
+﻿namespace AutomicObjectDesigner.Models.Objects;
+
+public class SapJobBwChainStep1Dto
 {
-    public class SapJobBWChainDto
-    {
-        public string SapSid { get; set; }
-        public string SapClient { get; set; }
-        public string SapKette { get; set; }
-        public bool RoutineJob { get; set; }
-        public bool DeleteSapJob { get; set; }
-        public string ProcessName { get; set; }
-        public string SapJobName { get; set; }
-    }
+    public string SapClient { get; set; }
+    public string SapSid { get; set; }
+    public string Kette { get; set; }
+    public bool RoutineJob { get; set; }
+    public string ProcessName { get; set; }
+    public string SapJobName { get; set; }
+    public bool DeleteSapJob { get; set; }
+    public string ObjectName { get; set; }
+    public string SapReport { get; set; }
+
+    public string SapVariant { get; set; }
+}
+
+public class SapJobBwChainStep2Dto
+{
+
+}
+public class SapJobBwChainStep3Dto
+{
+    public string Documentation { get; set; }
+}
+public class SapJobBwChainStep4Dto
+{
+
 }
