027c36fe9998239c14af3868962ec7343e5ae605
Tomasz Królicki <tomek.krolicki@gmail.com>
2022-09-15 14:11:48 +0200

    Migration

--- AutomicObjectDesigner/AutomicObjectDesignerBack/Migrations/20220915120753_Initial.Designer.cs
+++ AutomicObjectDesigner/AutomicObjectDesignerBack/Migrations/20220915120753_Initial.Designer.cs
@@ -4,6 +4,7 @@ using AutomicObjectDesignerBack.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AutomicObjectDesignerBack.Migrations
 {
     [DbContext(typeof(AppDatabaseContext))]
-    partial class AppDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220915120753_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
