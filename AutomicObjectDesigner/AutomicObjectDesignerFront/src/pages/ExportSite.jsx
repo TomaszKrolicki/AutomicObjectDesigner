6dd37c1a59a5d2333aeb85b5bbc1fce31be35d54
Leon Stawecki <stawecki.media@gmail.com>
2022-09-23 14:56:34 +0200

    Export front site is finished

--- AutomicObjectDesigner/AutomicObjectDesignerFront/src/pages/ExportSite.jsx
+++ AutomicObjectDesigner/AutomicObjectDesignerFront/src/pages/ExportSite.jsx
@@ -15,15 +15,16 @@ export const ExportSite = () => {
   //  const { state } = 1;
   //console.log("Export site - Id: " + state);
 
-const url = 'https://localhost:7017/api/'+ state.type + state.num;
-console.log("Fetching url: "+ url)
+  const url = 'https://localhost:7017/api/' + state.type + state.num;
+  console.log("Fetching url: " + url)
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const result = await fetch( url );
+        const result = await fetch(url);
         const dat = await result.json();
         if (dat != null) {
           console.log(dat);
+          document.getElementById("object").innerText = JSON.stringify(dat);
         } else {
           console.warn("Data couldn't be fetched");
         }
@@ -50,14 +51,21 @@ console.log("Fetching url: "+ url)
   return (
     <div className='md:px-4 py-2.5 container w-800'>
       <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 my-3">
-        This is a message
+        This is the object with your paramaters:
       </p>
-      <div>
-        
+      <div className='mp-2 font-blackcolumns-2 dark:text-gray-300' id='object'>
       </div>
-        <button onClick={handleClick} type="button" className="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
-         focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-right dark:bg-blue-600 dark:hover:bg-blue-700
-          dark:focus:ring-blue-800">Export</button>
+      <div className='my-10'>
+      <button onClick={handleClick} type="button" className="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+      focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-right dark:bg-blue-600 dark:hover:bg-blue-700
+      dark:focus:ring-blue-800">Export XML</button>
+      <button onClick={handleClick} type="button" className="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+      focus:ring-blue-300 font-medium rounded-lg mx-4 text-sm w-full sm:w-auto px-5 py-2.5 text-right dark:bg-blue-600 dark:hover:bg-blue-700
+      dark:focus:ring-blue-800">Export JSON</button>
+      <button onClick={handleClick} type="button" className="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+      focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-right dark:bg-blue-600 dark:hover:bg-blue-700
+      dark:focus:ring-blue-800">Export CSV</button>
+      </div>
     </div>
   )
 }
