64e47acc04a7cdfc8061b393728ec752c7937465
Leon Stawecki <stawecki.media@gmail.com>
2022-10-10 13:10:19 +0200

    Added user profile controler

---
+++ AutomicObjectDesigner/AutomicObjectDesignerBack/Controllers/UserProfile.cs
@@ -0,0 +1,53 @@
+﻿using AutomicObjectDesignerBack.Controllers.ObjectContollers;
+using AutomicObjectDesignerBack.Data;
+using AutomicObjectDesignerBack.Models.Objects;
+using AutomicObjectDesignerBack.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutomicObjectDesignerBack.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "User,Admin")]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly ILogger<UserProfileController> _logger;
+        private readonly IWindowsGeneralRepository _windowsGeneralRepository;
+        private readonly IUnixGeneralRepository _unixGeneralRepository;
+        private readonly ISapSimpleRepository _sapSimpleRepository;
+        private readonly ISapJobBwChainRepository _sapJobBwChainRepository;
+
+        public UserProfileController(AppDatabaseContext context, ILogger<UserProfileController> logger,
+            IWindowsGeneralRepository windowsRepository, IUnixGeneralRepository unixGeneralRepository,
+            ISapSimpleRepository sapSimpleRepository, ISapJobBwChainRepository sapJobBwChainRepository)
+        {
+            _windowsGeneralRepository = windowsRepository;
+            _unixGeneralRepository = unixGeneralRepository;
+            _sapSimpleRepository = sapSimpleRepository;
+            _sapJobBwChainRepository = sapJobBwChainRepository;
+            _logger = logger;
+        }
+        // GET  https://localhost:7017/api/UserProfile
+        [HttpGet]
+        public async Task<ActionResult<List<Object>>> GetFiles()
+        {
+            _logger.LogInformation("UserProfile called...");
+
+            var windowsGeneral = _windowsGeneralRepository.GetAll();
+            var unixGeneral = _unixGeneralRepository.GetAll();
+            var sapSimple = _sapSimpleRepository.GetAll();
+            var sapJobBwChain = _sapJobBwChainRepository.GetAll();
+
+            var x = new List<object>();
+            x.Add(windowsGeneral);
+            x.Add(unixGeneral);
+            x.Add(sapSimple);
+            x.Add(sapJobBwChain);
+
+            return Ok(x);
+        }
+    }
+}
+
+
