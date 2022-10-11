b540a606e3d22bf510560685ae9d4afbc8ce8b98
Leon Stawecki <stawecki.media@gmail.com>
2022-10-11 09:24:39 +0200

    Refactor

---
+++ AutomicObjectDesigner/AutomicObjectDesignerBack/Models/List/ListObjects.cs
@@ -0,0 +1,13 @@
+﻿using AutomicObjectDesigner.Models.Objects;
+using AutomicObjectDesignerBack.Models.Objects;
+
+namespace AutomicObjectDesignerBack.Models.List;
+
+public class ListObjects
+{
+    public List<WindowsGeneral>? WindowsGenerals { get; set; }
+    public List<SapJobBwChain>? SapJobBwChains { get; set; }
+    public List<UnixGeneral>? UnixGenerals { get; set; }
+    public List<SapSimple>? SapSimples { get; set; }
+
+}
